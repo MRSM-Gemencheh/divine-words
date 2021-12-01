a19ed6463f1b630921c404d00dccb332c560d6d6
MagusWyvern <72914828+MagusWyvern@users.noreply.github.com>
2021-12-02 07:42:10 +0800

    :stars: Surah Summary page commit

--- hadith.js
+++ hadith.js
@@ -1,40 +1,65 @@
 const hadith = {
 
+
     1: {
-        "title":" لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا",
+        "title": " لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا",
         "content": "Allah does not burden a soul beyond that it can bear (2:286)",
-        "category": "patience",   
+        "category": "patience",
     },
 
     2: {
-        "title" : " وَمَنْ يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ "
-        "content": "The wings of wisdom are no match for the wings of war.",
-        "category": "cleanliness",   
+        "title": " وَمَنْ يَتَوَكَّلْ عَلَى اللَّهِ فَهُوَ حَسْبُهُ ",
+        "content": "Whoever puts his trust in Allah; He will be enough for Him (65:3)",
+        "category": "cleanliness",
     },
 
     3: {
-        "title":"لَا تَهِنُوا وَلَا تَحْزَنُوا وَأَنْتُمُ الْأَعْلَوْنَ إِنْ كُنْتُمْ مُؤْمِنِينَ",
-        "content": "Knowledge is a flame in the darkness.",
-        "category": "patience",   
+        "title": "لَا تَهِنُوا وَلَا تَحْزَنُوا وَأَنْتُمُ الْأَعْلَوْنَ إِنْ كُنْتُمْ مُؤْمِنِينَ",
+        "content": "Do not lose hope, nor be sad. You will surely be victorious if you are true believers (3:139)",
+        "category": "patience",
     },
 
     4: {
-        "title":" مَاۤ اَصَابَ مِنۡ مُّصِيۡبَةٍ اِلَّا بِاِذۡنِ اللّٰهِ ؕ وَمَنۡ يُّؤۡمِنۡۢ بِاللّٰهِ يَهۡدِ قَلۡبَهٗ ؕ وَاللّٰهُ بِكُلِّ شَىۡءٍ عَلِيۡمٌ",
-        "content": "Even in the darkest of times, there is always hope.",
-        "category": "patience",   
+        "title": " مَاۤ اَصَابَ مِنۡ مُّصِيۡبَةٍ اِلَّا بِاِذۡنِ اللّٰهِ ؕ وَمَنۡ يُّؤۡمِنۡۢ بِاللّٰهِ يَهۡدِ قَلۡبَهٗ ؕ وَاللّٰهُ بِكُلِّ شَىۡءٍ عَلِيۡمٌ",
+        "content": "No misfortune ever befalls except by permission of Allah. And whoever has faith in Allah - He will guide his heart. And Allah is Knowing of all things (64:11)",
+        "category": "patience",
     },
 
     5: {
-        "title":" يَا أَيُّهَا الَّذِينَ آمَنُواْ اسْتَعِينُواْ بِالصَّبْرِ وَالصَّلاَةِ إِنَّ اللّهَ مَعَ الصَّابِرِينَ",
-        "content": "Do not fight fire with fire.",
-        "category": "cleanliness",   
+        "title": " يَا أَيُّهَا الَّذِينَ آمَنُواْ اسْتَعِينُواْ بِالصَّبْرِ وَالصَّلاَةِ إِنَّ اللّهَ مَعَ الصَّابِرِينَ",
+        "content": "O you who have believed, seek help through patience and prayer. Indeed, Allah is with the patient (2:153)",
+        "category": "cleanliness",
     },
 
     6: {
-        "title":" يَّهۡدِىۡ بِهِ اللّٰهُ مَنِ اتَّبَعَ رِضۡوَانَهٗ سُبُلَ السَّلٰمِ وَيُخۡرِجُهُمۡ مِّنَ الظُّلُمٰتِ اِلَى النُّوۡرِ بِاِذۡنِهٖ وَيَهۡدِيۡهِمۡ اِلٰى صِرَاطٍ مُّسۡتَقِيۡمٍ",
-        "content": "Appear weak when you are strong, appear strong when you are weak.",
-        "category": "patience",   
+        "title": " يَّهۡدِىۡ بِهِ اللّٰهُ مَنِ اتَّبَعَ رِضۡوَانَهٗ سُبُلَ السَّلٰمِ وَيُخۡرِجُهُمۡ مِّنَ الظُّلُمٰتِ اِلَى النُّوۡرِ بِاِذۡنِهٖ وَيَهۡدِيۡهِمۡ اِلٰى صِرَاطٍ مُّسۡتَقِيۡمٍ",
+        "content": "Through which Allah shows to all who seek to please Him the paths leading to safety. He brings them out, by His leave, from darkness to light and directs them on to the straight way (5:16)",
+        "category": "patience",
     },
+
+    7: {
+        "title": "وَاٰتٰٮكُمۡ مِّنۡ كُلِّ مَا سَاَلۡـتُمُوۡهُ  ؕ وَاِنۡ تَعُدُّوۡا نِعۡمَتَ اللّٰهِ لَا تُحۡصُوۡهَا ؕ اِنَّ الۡاِنۡسَانَ لَـظَلُوۡمٌ كَفَّارٌ",
+        "content": "And Who gave you all that you asked Him for. Were you to count the favours of Allah you shall never be able to encompass them. Verily man is highly unjust, exceedingly ungrateful (14:34)",
+        "category": "",
+    },
+
+    8: {
+        "title": "فَاِنَّ مَعَ الۡعُسۡرِ يُسۡرًا اِنَّ مَعَ الۡعُسۡرِ يُسۡرًا",
+        "content": "Indeed, there is ease with hardship (94:5) Most certainly, there is ease with hardship (94:6)",
+        "category": "",
+    },
+
+    9: {
+        "title": "قُلۡ يٰعِبَادِىَ الَّذِيۡنَ اَسۡرَفُوۡا عَلٰٓى اَنۡفُسِهِمۡ لَا تَقۡنَطُوۡا مِنۡ رَّحۡمَةِ اللّٰهِ  ؕ اِنَّ اللّٰهَ يَغۡفِرُ الذُّنُوۡبَ جَمِيۡعًا  ؕ اِنَّهٗ هُوَ الۡغَفُوۡرُ الرَّحِيۡم",
+        "content": "Tell them, (O Prophet): “My servants who have committed excesses against themselves, do not despair of Allah's Mercy. Surely Allah forgives all sins. He is Most Forgiving, Most Merciful (39:53)",
+        "category": "",
+    },
+       
+    10: {
+        "title": "اِنَّ هٰذَا كَانَ لَـكُمۡ جَزَآءً وَّكَانَ سَعۡيُكُمۡ مَّشۡكُوۡرًا",
+        "content": "Behold, this is your recompense and your endeavour has been appreciated",
+        "category": "",
+    }
 
     
 
--- surah.js
+++ surah.js
@@ -1,21 +1,91 @@
 const surah = {
 
-    "first": {
-        "title":" لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا",
-        "content": "Allah does not burden a soul beyond that it can bear (2:286)",
+    1: {
+        "title":"Surah Al-Asr ",
+        "content": "This sura teaches that all human beings are in loss, except those who have iman (faith in Islam), do righteous deeds and remind others of the Haqq (truth, rights, reality) and remind others of Sabr (patience).[9][10]",
+
+
+//         "The text of the Sura can be translated as the following:",
+        
+        
+// "        1.|وَالْعَصْرِ‌",
+        
+//         "By time (it explain that God swears By time).",
+        
+        
+//        " 2. إِنَّ الْإِنسَانَ لَفِي خُسْرٍ",‌
+        
+        
+//         "Indeed man is in loss,",
+        
+        
+//         // "3. إِلَّا الَّذِينَ آمَنُوا وَعَمِلُوا الصَّالِحَاتِ وَتَوَاصَوْا بِالْحَقِّ وَتَوَاصَوْا ,بِالصَّبْرِ"‌‌
+        
+        
+//         "except those who have faith and do righteous deeds, and enjoin one another to [follow] the truth, and enjoin one another to patience.",
+        
+        
+//         "What can we apply by this surah?" ,
+        
+        
+//         "- we should not waste time by doing unuseful stuff.",
         "category": "patience",   
     },
 
     2: {
         "title":" لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا",
-        "content": "Allah does not burden a soul beyond that it can bear (2:286)",
+        "content": " in Surah al-Kahf, we find that Allah offers examples of various people who did follow guidance, and how it saved them from suffering the same fate as the disbelievers.        This serves as yet another proof of the Qur’an being the true guidance, as mentioned in the first ten verses of the surah. Following the guidance of the Qur’an will lead to salvation, just as Divine guidance of the past led the people mentioned in this surah to their salvation.        Beside, in this surah, it demonstrates how the actions of believers lead to heavenly bliss, while the actions of those who are disbelieving and ungrateful to Allah only lead to disappointment and failure. ",
+        
+        // "As we arrive at the story of Musa and al-Khiḍr, we will learn how Divine guidance is upheld by a person with knowledge. In this section we learn that human beings cannot judge Allah’s decree based on what things appear to be at face value. Here we also learn that those people whom Allah has given resources and power must use these means to provide justice where it is needed and protect the innocent from oppressive people. ",
+        
         "category": "patience",   
     },
 
-    3: {
-        "title":" لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا",
-        "content": "Allah does not burden a soul beyond that it can bear (2:286)",
-        "category": "patience",   
-    },
 
 }
+
+let counter = 1
+
+
+
+const app = Vue.createApp   ({
+
+    data() {
+        return {
+            "title":" لَا يُكَلِّفُ اللَّهُ نَفْسًا إِلَّا وُسْعَهَا",
+            "content": "Choose the surah you want below",
+            "category": "patience",   
+        }
+    },
+
+    
+
+    methods: {
+        async getAlAsr() {
+            
+            // Fetch from data in hadith const
+            // this.title = hadith[counter].title
+            // this.content = hadith[counter].content
+            // this.category = hadith[counter].category
+
+            this.title = surah[1].title
+            this.title = surah[1].content
+
+            // if (counter == 6) {
+            //     counter = counter - 5
+            //     console.log(counter)
+            // } else {
+            //     counter = counter + 1
+            //     console.log(counter)
+            // }
+        },
+
+        async getAlKahfi() {
+
+            this.title = "Al Kahfi"
+            this.title = surah[2].content
+        }
+    }
+})
+
+app.mount('#app')
